c8edccecd1668c5f5cf662e64265ed4c833aee8f
Yngvarus <103531512+Yngvarus@users.noreply.github.com>
2024-04-15 14:58:14 +0200

    button with relative function initialized

--- src/script.js
+++ src/script.js
@@ -1,14 +1,24 @@
 const container = document.getElementById('container');
 
+function handleMouseOver() {
+  if (!this.classList.contains('hovered')) {
+    this.classList.add('hovered');
+    this.style.backgroundColor = 'yellow';
+  }
+}
+
 // for loop to add 16x16 squares
 for (let i = 0; i < 256; i++) {
   const square = document.createElement('div');
   square.classList.add('square');
-  
+
   //change background when mouse hover
-  square.addEventListener('mouseover', () => {
-    square.classList.add('hovered');
-  });
+  square.addEventListener('mouseover', handleMouseOver);
 
   container.appendChild(square);
+}
+
+function changeGrid(){
+  let num = prompt("Please enter a number of squares for your grid (between 4 and 100)");
+  
 }
